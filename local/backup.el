86312c4d5befb22eba2ffd47508b3fe1a93dfb3b
Dan Lentz <danlentz@gmail.com>
2015-09-09 19:42:46 -0400

    new: autosave dir

--- local/backup.el
+++ local/backup.el
@@ -1,7 +1,16 @@
 
 
+;; Backup Directory
+
 (add-to-list 'backup-directory-alist '("." . "~/tmp/"))
 ;; (setq version-control t)
 
+
+;; Autosave
+
+(defvar autosave-dir  (expand-file-name "~/tmp/autosave/"))
+(make-directory autosave-dir t)
+(setq auto-save-file-name-transforms
+      `(("\\(?:[^/]*/\\)*\\(.*\\)" ,(concat autosave-dir "\\1") t)))
 
 (provide 'backup)
